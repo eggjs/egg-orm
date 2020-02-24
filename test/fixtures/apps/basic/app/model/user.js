5a8f304177d59381391e890d92f9e7acd923ca76
fengmk2 <fengmk2@gmail.com>
2020-02-24 20:41:28 +0800

    fix: run in sequelize mode (#3)

--- test/fixtures/apps/basic/app/model/user.js
+++ test/fixtures/apps/basic/app/model/user.js
@@ -1,14 +1,15 @@
 'use strict';
 
 module.exports = (app, Model) => {
-  const { STRING } = Model;
+  // const { STRING } = Model;
 
   class User extends Model {
-    static get schema() {
-      return {
-        name: { type: STRING, allowNull: false },
-      };
-    }
+    // static get schema() {
+    //   return {
+    //     nickname: { type: STRING, allowNull: false },
+    //     email: { type: STRING, allowNull: true },
+    //   };
+    // }
   }
 
   return User;
--- test/fixtures/apps/basic/config/config.js
+++ test/fixtures/apps/basic/config/config.js
@@ -2,4 +2,11 @@
 
 exports.orm = {
   database: 'egg-orm',
+  sequelize: true,
+};
+
+exports.keys = 'hello';
+
+exports.security = {
+  csrf: false,
 };
