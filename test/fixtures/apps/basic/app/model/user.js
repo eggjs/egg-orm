083f04a354ea5dec3f7695547ac12f6774ff5917
fengmk2 <fengmk2@gmail.com>
2020-02-24 19:50:06 +0800

    f

--- test/fixtures/apps/basic/app/model/user.js
+++ test/fixtures/apps/basic/app/model/user.js
@@ -1,15 +1,15 @@
 'use strict';
 
 module.exports = (app, Model) => {
-  const { STRING } = Model;
+  // const { STRING } = Model;
 
   class User extends Model {
-    static get schema() {
-      return {
-        nickname: { type: STRING, allowNull: false },
-        email: { type: STRING, allowNull: true },
-      };
-    }
+    // static get schema() {
+    //   return {
+    //     nickname: { type: STRING, allowNull: false },
+    //     email: { type: STRING, allowNull: true },
+    //   };
+    // }
   }
 
   return User;
